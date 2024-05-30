1bbe80a8ea72da27cdb98bbfe6d291db78c0df84
Aktonen <aku.tolonen1999@hotmail.fi>
2024-05-30 16:08:13 +0300

    Initialized the project and added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native-community",
+    "plugin:react/recommended",
+  ],
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 2021,
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true
+    }
+  },
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "react/prop-types": "off",
+    "prettier/prettier": "off",
+  },
+};
